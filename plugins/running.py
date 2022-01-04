902b0fb1c33cf70e52731e2cf4b9813a7db09445
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2022-01-04 16:02:06 +0530

    Update running.py

--- plugins/running.py
+++ plugins/running.py
@@ -63,7 +63,7 @@ async def run(bot, message):
             pass
    # await m.delete()
     buttons = [[
-        InlineKeyboardButton('📜 Support Group', url='https://t.me/DxHelpDesk')
+        InlineKeyboardButton('📜 Support Group', url='https://t.me/M_STER_TECH_GROUP')
     ]] 
     reply_markup = InlineKeyboardMarkup(buttons)
     await m.edit(
